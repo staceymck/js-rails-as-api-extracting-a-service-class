ecaba27e01f30cda8ebadbaca903222ccab4f0a8
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-20 16:44:33 -0500

    commit tree 7b50a243b580064f37f1f99c634e86294023a0c7

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,6 +1,15 @@
 class SightingsController < ApplicationController
+    
+    def index
+      sightings = Sighting.all
+      render json: SightingSerializer.new(sightings).to_serialized_json
+    end
+
     def show
-        @sighting = Sighting.find(params[:id])
-        render json: @sighting.to_json(:include => {:bird => {:only =>[:name, :species]}, :location => {:only =>[:latitude, :longitude]}}, :except => [:updated_at])
+      sighting = Sighting.find(params[:id])
+        if sighting
+          render json: SightingSerializer.new(sighting).to_serialized_json
+        else
+          render json: {message: 'No sighting with that id found'}
     end
 end
